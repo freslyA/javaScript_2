adea0d3adef068e528fa0e5f2a517d85014bd1a3
freslyA <freslyandino@gmail.com>
2026-04-26 13:20:27 -0500

    los ejercicios for funcionan correctamente

---
+++ ejercicios_for/ejercicio.js
@@ -0,0 +1,20 @@
+function listarNumeros(){
+    for(let i=0;i<=3;i++){
+        console.log(i)
+    }
+}
+function listarNumerosReversa(){
+    for(let i=3;i>0;i--){
+        console.log(i)
+    }
+}
+function listarNumerosPares(){
+    for(let i=0;i<10;i+=2){
+        console.log(i)
+    }
+}
+function listarNumerosImpares(){
+    for(let i=1;i<=7;i+=2){
+        console.log(i)
+    }
+}
