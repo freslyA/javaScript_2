92d552a109384c23950b5862ca59b25c91067c4a
freslyA <freslyandino@gmail.com>
2026-05-13 18:09:19 -0500

    la funcion de guardar y pintar cliente funciona correctamente

--- simulador2/simulador_completo.js
+++ simulador2/simulador_completo.js
@@ -25,5 +25,39 @@ function guardarTasa(){
     alert("la tasa debe estar entre 10% y 20%")
   }
 }
+function guardarCliente(){
+  let cliente= {}
+  cedula=recuperaraTexto("txtCedula")
+  nombre=recuperaraTexto("txtNombre")
+  apellido=recuperaraTexto("txtApellido")
+  ingresos=recuperarFloat("txtIngresos")
+  egresos=recuperarFloat("txtEgresos")
+  cliente.cedula=cedula
+  cliente.nombre=nombre
+  cliente.apellido=apellido
+  cliente.ingresos=ingresos
+  cliente.egresos=egresos
+  clientes.push(cliente)
+  pintarCliente()
+}
+function pintarCliente(){
+  let tabla=document.getElementById("tablaClientes")
+  let contenido=""
+  for(i=0;i<clientes.length;i++){
+    elemento=clientes[i]
+    contenido+= "<tr>"+
+          "<td>"+elemento.cedula+"</td>"+
+          "<td>"+elemento.nombre+"</td>"+
+          "<td>"+elemento.apellido+"</td>"+
+          "<td>"+elemento.ingresos+"</td>"+
+          "<td>"+elemento.egresos+"</td>"+
+          "<td>"+
+            "<button>Actualizar</button>"+
+            "<button>Eliminar</button>"+
+          "</td>"+
+        "</tr>"
+  }
+  tabla.innerHTML=contenido
+}
   
 //Para recuperar o mostrar información usar los métodos de la clase utilitarios, puede agregar métodos adicionales en utilitarios
